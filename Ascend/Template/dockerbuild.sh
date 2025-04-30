93a17ddc4a98d38af6f63244dee6c6038872f19d
stephenfuyuxin <121746060+stephenfuyuxin@users.noreply.github.com>
2025-04-30 16:19:11 +0800

    Create dockerbuild.sh
    init

---
+++ Ascend/Template/dockerbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build \
+--build-arg no_proxy=127.0.0.1,localhost,local,.local,172.17.0.1 \
+--build-arg DEVICE=910b \
+--build-arg ARCH=aarch64 \
+--build-arg CANN_VERSION=8.0.0 \
+--build-arg PYTHON_VERSION=3.10.14 \
+--build-arg PY_VERSION=310 \
+--build-arg TORCH_VERSION=2.1.0 \
+--build-arg MINDIE_VERSION=1.0.0 \
+-t <image-name>:<image-tag> \
+--target <stage> .
