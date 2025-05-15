01f2905a293fea318d6f0019f0d27cc59f504bfd
stephenfuyuxin <121746060+stephenfuyuxin@users.noreply.github.com>
2025-05-15 17:16:12 +0800

    Add files via upload

---
+++ Ascend/LivePortrait/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+docker run -it --ipc=host --net=host --name zhaohang-hwhiaiuser-liveportrait-fuyuxin-npuid7 \
+--device=/dev/davinci7 \
+--device=/dev/davinci_manager \
+--device=/dev/devmm_svm \
+--device=/dev/hisi_hdc \
+-v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+-v /usr/local/Ascend/add-ons/:/usr/local/Ascend/add-ons/ \
+-v /usr/local/sbin/npu-smi:/usr/local/sbin/npu-smi \
+-v /usr/local/sbin/:/usr/local/sbin/ \
+-v /var/log/npu/conf/slog/slog.conf:/var/log/npu/conf/slog/slog.conf \
+-v /var/log/npu/slog/:/var/log/npu/slog \
+-v /var/log/npu/profiling/:/var/log/npu/profiling \
+-v /var/log/npu/dump/:/var/log/npu/dump \
+-v /var/log/npu/:/usr/slog \
+-v /data:/data \
+-v /nfs:/nfs \
+zhaohang-hwhiaiuser-liveportrait:1 \
+/bin/bash
